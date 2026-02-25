a48f5e52041f51fa946237d15f11a217ecd2ab59
mamonde456 <mamonde456@gmail.com>
2026-02-25 10:21:16 +0900

    feat: 카테고리 사이드 메뉴 추가

---
+++ src/feature/common/MainMenu.tsx
@@ -0,0 +1,34 @@
+import useUIStore, { type TITLE_TYPE } from "../lib/store/useUIStore";
+
+const MENU_LIST = [
+  { id: 0, category: "회원 관리" },
+  { id: 1, category: "출결 관리" },
+  { id: 2, category: "세션 관리" },
+];
+
+export default function MainMenu() {
+  const selected = useUIStore((state) => state.selectedCategory);
+  return (
+    <div className="flex-1 bg-gray-300">
+      <div className="p-5">
+        <img src="/logo.webp"></img>
+      </div>
+      <nav className="p-5">
+        <ul>
+          {MENU_LIST.map((el) => (
+            <li
+              className="p-2 mb-2"
+              key={el.id}
+              onClick={() => {
+                selected(el.category as TITLE_TYPE);
+                console.log(el.category);
+              }}
+            >
+              {el.category}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
