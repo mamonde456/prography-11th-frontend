e785675beca1290c81914a98e52ab3d45f5821f4
mamonde456 <mamonde456@gmail.com>
2026-02-26 21:08:42 +0900

    feat: 출결 UI 상태 추가

--- src/feature/lib/store/useUIStore.tsx
+++ src/feature/lib/store/useUIStore.tsx
@@ -4,23 +4,28 @@ export type TITLE_TYPE = "회원 관리" | "출결 관리" | "세션 관리";
 
 type State = {
   title: TITLE_TYPE;
-  user: number | null;
+  userId: number | null;
   isCreateMemberView: boolean;
+  attendancesCategory: string;
 };
 
 type Action = {
   selectedCategory: (title: TITLE_TYPE) => void;
-  selectedUser: (user: number | null) => void;
+  selectedUser: (userId: number | null) => void;
   setIsCreateMemberView: (state: boolean) => void;
+  setAttendancesCategory: (state: string) => void;
 };
 
 const useUIStore = create<State & Action>((set) => ({
   title: "회원 관리",
-  user: null,
+  userId: null,
   isCreateMemberView: false,
+  attendancesCategory: "",
   selectedCategory: (title) => set(() => ({ title })),
-  selectedUser: (user) => set(() => ({ user })),
+  selectedUser: (userId) => set(() => ({ userId })),
   setIsCreateMemberView: (state) => set(() => ({ isCreateMemberView: state })),
+  setAttendancesCategory: (state) =>
+    set(() => ({ attendancesCategory: state })),
 }));
 
 export default useUIStore;
