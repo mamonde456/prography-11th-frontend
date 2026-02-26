73d84c3708388a6cd7402894928e539beea70a84
mamonde456 <mamonde456@gmail.com>
2026-02-26 21:19:03 +0900

    fix: 중복 url 분리

--- src/feature/attendance/const/urls.ts
+++ src/feature/attendance/const/urls.ts
@@ -1,9 +1,9 @@
-const BACKEND_URL = "/api/v1";
+import { BACEND_ADMIN_URL } from "../../common/const/urls";
 
 export const ADMIN_ATTENDANCES_MEMBER_LIST =
-  import.meta.env.VITE_BASE_URL + BACKEND_URL + "/admin/attendances/members";
+  import.meta.env.VITE_BASE_URL + BACEND_ADMIN_URL + "/attendances/members";
 
 export const ADMIN_ATTENDANCES =
-  import.meta.env.VITE_BASE_URL + BACKEND_URL + "/admin/attendances";
+  import.meta.env.VITE_BASE_URL + BACEND_ADMIN_URL + "/attendances";
 export const ADMIN_COHORT_MEMBERS =
-  import.meta.env.VITE_BASE_URL + BACKEND_URL + "/admin/cohort-members";
+  import.meta.env.VITE_BASE_URL + BACEND_ADMIN_URL + "/cohort-members";
