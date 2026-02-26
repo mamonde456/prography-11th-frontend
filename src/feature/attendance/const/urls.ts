195bcc1cb22b7861634ba2b2c967fdc82caaa111
mamonde456 <mamonde456@gmail.com>
2026-02-26 21:02:44 +0900

    feat: 출결 상수 url 추가

---
+++ src/feature/attendance/const/urls.ts
@@ -0,0 +1,9 @@
+const BACKEND_URL = "/api/v1";
+
+export const ADMIN_ATTENDANCES_MEMBER_LIST =
+  import.meta.env.VITE_BASE_URL + BACKEND_URL + "/admin/attendances/members";
+
+export const ADMIN_ATTENDANCES =
+  import.meta.env.VITE_BASE_URL + BACKEND_URL + "/admin/attendances";
+export const ADMIN_COHORT_MEMBERS =
+  import.meta.env.VITE_BASE_URL + BACKEND_URL + "/admin/cohort-members";
